88178f87de44e5adc5432a45d69173a482dd24e2
USTS <usts@usts.ai>
2025-04-25 10:00:54 +0200

    ddde

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import ArticlesSection from './components/ArticlesSection';
+import TemoignagesSection from './components/TemoignagesSection';
+import ActualitesSection from './components/ActualitesSection';
+import AbonnementsSection from './components/AbonnementsSection';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <Hero />
+        <ArticlesSection />
+        <TemoignagesSection />
+        <ActualitesSection />
+        <AbonnementsSection />
+      </main>
+      <Footer />
     </div>
   );
 }
